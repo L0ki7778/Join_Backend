76c5a81786fa3768a9480fa2d493d9cd184ddcfa
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-03-07 21:52:24 +0100

    added userviewset to routing

--- ticket/api/urls.py
+++ ticket/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework import routers
-from .views import TicketViewSet
+from .views import TicketViewSet,UserViewSet
 
 router = routers.SimpleRouter()
 router.register(r'tickets', TicketViewSet)
+router.register(r'user', UserViewSet)
 
 urlpatterns = router.urls
