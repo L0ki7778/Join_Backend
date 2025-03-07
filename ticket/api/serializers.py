e46e15ca7698db047b72962b6fc7119d467c4df4
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-03-07 23:34:35 +0100

    added namefilter for assignee id in ticket

--- ticket/api/serializers.py
+++ ticket/api/serializers.py
@@ -5,9 +5,18 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields = '__all__'
-        
+            
         
 class TicketSerializer(serializers.ModelSerializer):
+    
+    
+    assignees = serializers.SerializerMethodField()
+    
     class Meta:
         model=Ticket
         fields = '__all__'
+        
+    def get_assignees(self,obj):
+        users = User.objects.filter(id__in=obj.assignees.all())
+        return [user.name for user in users]
+        
