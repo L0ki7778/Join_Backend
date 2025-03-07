e45157676d46ff6400624672198702994e1f6ac3
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-03-07 23:34:01 +0100

    added serializer to viewsets

--- ticket/api/views.py
+++ ticket/api/views.py
@@ -1,11 +1,13 @@
 from rest_framework.viewsets import *
 from ..models import Ticket, User
+from .serializers import TicketSerializer,UserSerializer
 
 class TicketViewSet(ModelViewSet):
-    serializer_class = ""
+    serializer_class = TicketSerializer
     queryset = Ticket.objects.all()
-    pass
+    
 
 class UserViewSet(ModelViewSet):
-    serializer_class=""
+    serializer_class=UserSerializer
     queryset = User.objects.all()
+    
